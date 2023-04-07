6a7ee45047315291bed191bbf2e2a9a28baa5031
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-04-07 17:06:38 +0200

    set okHttp library

--- app/src/main/java/com/example/clothingsuggester/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.clothingsuggester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clothingsuggester.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.get.setOnClickListener {
+            //getRequestUsingOkHttp()
+        }
     }
 }
