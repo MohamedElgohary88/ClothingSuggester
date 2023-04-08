9a7e331dbf601823c920e29711ba915cbffe5630
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-04-08 16:27:57 +0200

    convert json file

--- app/src/main/java/com/example/clothingsuggester/MainActivity.kt
+++ app/src/main/java/com/example/clothingsuggester/MainActivity.kt
@@ -3,47 +3,62 @@ package com.example.clothingsuggester
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.clothingsuggester.data.WeatherData
 import com.example.clothingsuggester.databinding.ActivityMainBinding
+import com.google.gson.Gson
 import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
-    val client = OkHttpClient()
+    lateinit var binding: ActivityMainBinding
+    val client = OkHttpClient.Builder().build()
     val LOG_TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.get.setOnClickListener {
-            getRequestUsingOkHttp()
+            getRequestUsingOkHttp(binding.country.query.toString())
         }
     }
-    private fun getRequestUsingOkHttp() {
-        val country = binding.country.text.toString()
+
+    private fun getRequestUsingOkHttp(country :String) {
         val url = HttpUrl.Builder()
             .scheme("http")
             .host("api.weatherstack.com")
             .addPathSegment("current")
-            .addQueryParameter("access_key", "YOUR_API_KEY")
+            .addQueryParameter("access_key", "262cdf4d8acfb7476a3849b41c90b3bc")
             .addQueryParameter("query", country)
             .build()
 
         val request = Request.Builder().url(url).build()
-        client.newCall(request).enqueue(object :Callback{
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 runOnUiThread {
-                    binding.textView.text = e.message.toString()
-                    Log.d(LOG_TAG,e.message.toString())
+                binding.textTemperature.text = e.message.toString()
+                    // Log.d(LOG_TAG,e.message.toString())
                 }
             }
 
             override fun onResponse(call: Call, response: Response) {
-                runOnUiThread {
-                    binding.textView.text = response.body?.string().toString()
-                    Log.e(LOG_TAG,response.body?.string().toString())
+                response.body?.string()?.let { jsonString ->
+                    val result = Gson().fromJson(jsonString,WeatherData::class.java)
+                    Log.i(LOG_TAG, result.toString())
+                    runOnUiThread {
+                        binding.textTemperature.text = result.current.temperature.toString()
+                        binding.textStatus.text = result.current.weather_descriptions.toString()
+                        binding.textCountry.text = result.location.name
+                        setWeatherIcons(result.current.weather_icons.toString(),binding.imageView)
+                    }
                 }
             }
         })
+    }
+    fun setWeatherIcons(imageUrl: String, imageView: ImageView) {
+        Glide.with(imageView.context).load(imageUrl).placeholder(R.drawable.ic_download)
+            .into(imageView)
     }
 }
